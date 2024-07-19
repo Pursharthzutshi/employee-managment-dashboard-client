12fb0fc240d799952a80004e9755bb24b4d46bd2
Pursharth Zutshi <pursharthzutshi@gmail.com>
2024-07-19 19:27:26 +0530

    commit

--- src/index.tsx
+++ src/index.tsx
@@ -3,49 +3,62 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from 'react-router-dom';
-// newFunction();
+
+import { ApolloClient, ApolloProvider, HttpLink, InMemoryCache, split } from '@apollo/client';
+import { getMainDefinition } from '@apollo/client/utilities';
+
+import { GraphQLWsLink } from "@apollo/client/link/subscriptions";
+import { createClient } from "graphql-ws";
 import { Provider } from 'react-redux';
-import { store } from '../src/ReduxStore/store';
-import { ApolloClient, InMemoryCache } from '@apollo/client';
-import { ApolloProvider } from '@apollo/client';
+import { store } from './ReduxStore/store';
+import { BrowserRouter } from 'react-router-dom';
 
+const webLink = new GraphQLWsLink(
+  createClient({
+    url: "ws://localhost:4004/graphql",
+  }),
+);
 
-// export const client = new ApolloClient({
-//   uri: 'http://localhost:4004/api/graphql',
-//   cache: new InMemoryCache(),
-// })
+const httpLink = new HttpLink({
+  uri: 'http://localhost:4004/graphql',
+});
 
+const link: any = split(
+  ({ query }) => {
+    const definition = getMainDefinition(query);
+    return (
+      definition.kind === 'OperationDefinition' &&
+      definition.operation === 'subscription'
+    );
+  },
+  webLink,
+  httpLink,
+);
 
 export const client = new ApolloClient({
-  uri: 'https://employee-managment-dashboard-server.vercel.app/api/graphql',
+  link,
   cache: new InMemoryCache(),
-})
+  connectToDevTools: true
+
+});
 
 
-
-const rootElement = document.getElementById('root') as HTMLElement;
-
-if (rootElement) {
-  const root = ReactDOM.createRoot(rootElement);
-
-  root.render(
-    <React.StrictMode>
-
-      <BrowserRouter>
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <ApolloProvider client={client}>
         <Provider store={store}>
-          <ApolloProvider client={client}>
-            <App />
-          </ApolloProvider>
+          <App />
         </Provider>
-      </BrowserRouter>
-    </React.StrictMode>
-  );
-} else {
-  console.error("Failed to find the root element.");
+      </ApolloProvider>
+    </BrowserRouter>
+  </React.StrictMode>
+);
 
-}
 // If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
+// to log results (for example: repor tWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
