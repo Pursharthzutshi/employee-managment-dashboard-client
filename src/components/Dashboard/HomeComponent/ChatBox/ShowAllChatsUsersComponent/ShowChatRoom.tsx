4f6935b2393c4c531a37ad7d1cfb63f134347e09
Pursharth Zutshi <pursharthzutshi@gmail.com>
2024-07-22 01:26:47 +0530

    commit

--- src/components/Dashboard/HomeComponent/ChatBox/ShowAllChatsUsersComponent/ShowChatRoom.tsx
+++ src/components/Dashboard/HomeComponent/ChatBox/ShowAllChatsUsersComponent/ShowChatRoom.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { useAppDispatch, useAppSelector } from "../../../../../ReduxHooks";
 import { FaCross, FaPaperPlane, FaSadCry, FaTimes } from "react-icons/fa";
 import { removeChatID, setChatID, setShowChatRoom } from "../../../../../ReduxSlicers/ShowChatRoomSlicer";
@@ -10,6 +10,12 @@ import image from "../../../../RegisterComponent/images/add-user.png"
 import "./ShowChatRoom.css"
 
 function ShowChatRoom() {
+    const messagesEndRef = useRef<HTMLDivElement | null>(null);
+
+    const messagesContainerRef = useRef<HTMLDivElement | null>(null);
+
+    const [showMessageName,setShowMessageName] = useState("")
+
     const showUserDetails = useAppSelector((state) => state.ShowChatRoomSlicer.showSelectedChatUserDetails)
 
     const senderID = useAppSelector((state) => state.ShowChatRoomSlicer.senderID)
@@ -20,36 +26,44 @@ function ShowChatRoom() {
     const [sendMessage] = useMutation(send_message_query, ({
         onCompleted: (data) => {
             // setChatMessage("")
-            console.log(data)
+            // console.log(data.sendMessage[0].senderId)
+            // if(data.sendMessage[0].senderId === senderID){
+            //     setShowMessageName("You")
+            // }
+
+            // if(data.sendMessage[0].senderId === receiverID){
+            //     setShowMessageName("Other")
+
+            // }
         },
         update: (cache, { data: { sendMessage } }) => {
-            console.log(sendMessage);
-            const newMessageData = sendMessage
+            const newMessageData = sendMessage;
 
-            const showFetchedChats: any = cache.readQuery(
-                {
+            if (newMessageData && newMessageData.message) { // Check if the message is valid
+                const showFetchedChats: any = cache.readQuery({
                     query: show_chat_room_query,
                     variables: {
                         showSenderReceiverChatParameters: {
                             senderId: senderID,
-                            receiverId: receiverID
-                        }
-                    }
-                }
-            )
-            if (senderID && receiverID) {
-                cache.writeQuery({
-                    query: show_chat_room_query,
-                    variables: {
-                        showSenderReceiverChatParameters: {
-                            senderId: senderID,
-                            receiverId: receiverID
-                        }
+                            receiverId: receiverID,
+                        },
                     },
-                    data: {
-                        showSenderReceiverChat: [...showFetchedChats.showSenderReceiverChat, newMessageData]
-                    }
-                })
+                });
+
+                if (senderID && receiverID) {
+                    cache.writeQuery({
+                        query: show_chat_room_query,
+                        variables: {
+                            showSenderReceiverChatParameters: {
+                                senderId: senderID,
+                                receiverId: receiverID,
+                            },
+                        },
+                        data: {
+                            showSenderReceiverChat: [...showFetchedChats.showSenderReceiverChat, newMessageData],
+                        },
+                    });
+                }
             }
         }
     }))
@@ -62,47 +76,51 @@ function ShowChatRoom() {
         },
     }
     ))
-
-    useSubscription(message_sent_subscribe, {
+useEffect(()=>{
+   
+})
+    const [subscription, setSubscription] = useState(null)
+    const newSubscription = useSubscription(message_sent_subscribe, {
         onSubscriptionData: ({ client, subscriptionData }) => {
             const newMessage = subscriptionData.data.messageSent;
+
+  
 
             client.writeQuery({
                 query: show_chat_room_query,
                 variables: {
                     showSenderReceiverChatParameters: {
                         senderId: senderID,
-                        receiverId: receiverID
-                    }
+                        receiverId: receiverID,
+                    },
                 },
                 data: {
                     showSenderReceiverChat: [
-                        ...(fetchSenderReceiverChatRoomData?.showSenderReceiverChat || []), // Use optional chaining to handle initial undefined state
-                        newMessage
-                    ]
+                        ...(fetchSenderReceiverChatRoomData?.showSenderReceiverChat || []),
+                        newMessage,
+                    ],
                 },
             });
-        },
-    });
+            if (messagesContainerRef.current) {
+                const { scrollTop, scrollHeight, clientHeight } = messagesContainerRef.current;
+                setShouldScrollToBottom(scrollTop + clientHeight >= scrollHeight);
+            }
+     
+        }
 
-    // useSubscription(message_sent_subscribe, {
-    //     onSubscriptionData: ({ client, subscriptionData }) => {
-    //         const newMessage = subscriptionData.data.messageSent;
-    //         console.log(newMessage)
-    //         client.writeQuery({
-    //             query: show_chat_room_query,
-    //             variables: {
-    //                 senderId: senderID,
-    //                 receiverId: receiverID
-    //             },
-    //             data: {
-    //                 showSenderReceiverChat: [...fetchSenderReceiverChatRoomData.showSenderReceiverChat, newMessage],
-    //             },
-    //         });
-    //     },
-    // });
+    },
+    )
+
 
     useEffect(() => {
+        if (messagesEndRef.current) {
+            messagesEndRef.current.scrollIntoView({ behavior: "smooth" });
+        }
+    }, [fetchSenderReceiverChatRoomData]);
+    useEffect(() => {
+        let unsubscribe;
+
+        console.log(newSubscription)
         if (senderID && receiverID) {
             showChatRoom({
                 variables: {
@@ -113,11 +131,36 @@ function ShowChatRoom() {
                 }
             })
         }
+        unsubscribe = newSubscription; // Assuming newSubscription returns an unsubscribe function
+
     }, [])
 
     const closeChatBox = () => {
         Dispatch(setShowChatRoom(false))
         Dispatch(removeChatID(""))
+    }
+
+    const sendMessageData = (e: any) => {
+        console.log(e)
+        if (chatMessage.trim()) {
+            if (e.key === "Enter") {
+                sendMessage({
+                    variables: {
+                        sendMessageParameters: {
+                            uid: uuidv4(),
+                            senderId: senderID,
+                            receiverId: receiverID,
+                            message: chatMessage
+                        }
+                    }
+                })
+                setChatMessage("")
+
+            }
+        } else {
+            console.log("worng")
+        }
+
     }
 
     const Dispatch = useAppDispatch()
@@ -145,43 +188,60 @@ function ShowChatRoom() {
                 <div className="show-chat-room-input-div">
 
                     <div className="chat-input-message-send-icon-div">
-                        <textarea className="font-semibold send-message-textarea" onChange={(e) => { setChatMessage(e.target.value) }} placeholder="Send Message" rows={3} cols={43}  ></textarea>
-                        {
-                            chatMessage ? <FaPaperPlane className="send-message-icon-button" onClick={() =>
-                                sendMessage({
-                                    variables: {
-                                        sendMessageParameters: {
-                                            uid: uuidv4(),
-                                            senderId: senderID,
-                                            receiverId: receiverID,
-                                            message: chatMessage
+                        <textarea value={chatMessage} onKeyUp={sendMessageData} className="font-semibold send-message-textarea" onChange={(e) => { setChatMessage(e.target.value) }} placeholder="Send Message" rows={4} cols={44}  >
+                            {/* {
+                                chatMessage ? <FaPaperPlane className="send-message-icon-button" onClick={() =>
+                                    sendMessage({
+                                        variables: {
+                                            sendMessageParameters: {
+                                                uid: uuidv4(),
+                                                senderId: senderID,
+                                                receiverId: receiverID,
+                                                message: chatMessage
+                                            }
                                         }
-                                    }
-                                })
-                            } /> : null
-                        }
+                                    })
+                                } /> : null
+                            } */}
+                        </textarea>
+
 
                     </div>
                 </div>
 
             </div>
 
-                <div className="messages-container">
-                    {
-                        fetchSenderReceiverChatRoomData && fetchSenderReceiverChatRoomData.showSenderReceiverChat.map((val: any) => {
-                            return (
-                                <div className="messages-div">
-                                    {
-                                        <p className="messages">{val.message}</p>
-                                    }
-                                </div>
-                            )
-                        })
-                    }
-                </div>
+            <div className="messages-container">
+                {
+                    fetchSenderReceiverChatRoomData && fetchSenderReceiverChatRoomData.showSenderReceiverChat.map((val: any) => {
+                    console.log(val)
+                        return (
+                            <div className="messages-div">
+                            
+                                    <img src={image}/>
+                                    <p>
+                                        {
+                                            val.senderId === senderID ? <p className="font-semibold">You</p>:<p className="font-semibold">{showUserDetails.name}</p>
+                                        }
+                                    </p>
+                                    <p className="font-semibold">{showMessageName}</p>
+                                    <p className="messages">{val.message}</p>
+                          
+                                <div ref={messagesEndRef} />
+
+                            </div>
+
+                        )
+                    })
+                }
+            </div>
 
         </div>
     )
 }
 
 export default ShowChatRoom;
+
+function setShouldScrollToBottom(arg0: boolean) {
+    throw new Error("Function not implemented.");
+}
