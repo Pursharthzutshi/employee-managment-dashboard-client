7b3761a8de2ca8adfe83650048a976e10fbf647a
Pursharthzutshi <pursharthzutshi@gmail.com>
2026-06-16 02:25:18 +0530

    fix: add missing alt attributes to images and remove unused react import in components

--- src/components/HomePageComponent/Privacy/Privacy.tsx
+++ src/components/HomePageComponent/Privacy/Privacy.tsx
@@ -5,7 +5,7 @@ function Privacy() {
     return (
         <section className="privacy-component">
             <p className="font-bold text-blue text-xl text-center">Your Privacy is our Privacy</p>
-            <img className="privacy-component-image" src={privacyIcon} />
+            <img className="privacy-component-image" src={privacyIcon} alt="Privacy" />
             <p className="text-blue font-semibold text-sl mt-10 text-center">We prioritize your privacy by implementing secure authentication methods. <br></br>Your personal data is protected with state-of-the-art encryption and privacy protocols. Proper Authentication is done whenever a user tries to login</p>
         </section>
     )
